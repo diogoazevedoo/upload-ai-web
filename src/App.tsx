20bad2b2d5c52dcaf1960cac491fc04b36096ba6
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-02-24 11:46:20 +0000

    UI: Header creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Github } from "lucide-react"
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Developed with ❤️ by Diogo Azevedo</span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
